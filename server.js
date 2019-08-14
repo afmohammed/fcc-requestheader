80be9018cc20630230841ab9fbcf2e22e50a9b8e
Glitch (root-concrete) <none>
2019-08-14 02:52:24 +0000

    🏉👗 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,43 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get("/api/whoami", function(req, res) {      
+  //res.json({"ipaddress:": req.headers.host});  
+    
+  var language = req.headers["accept-language"];
+  var userAgent = req.headers["user-agent"];
+  res.send({"ipaddress": req.connection.remoteAddress,"language":language.slice(0, language.indexOf(",")),
+        "software":userAgent.slice(userAgent.indexOf("(") + 1, userAgent.indexOf(")"))})
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
+//{"ipaddress":"159.20.14.100","language":"en-US,en;q=0.5",
+//"software":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"}
+//{"ipaddress":"root-concrete.glitch.m","language":"en-US","software":"X11; CrOS x86_64 12105.100.0"}
